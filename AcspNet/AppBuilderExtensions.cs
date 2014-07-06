ced00a74d911380650a1df439cc429f0f9f72ab2
Alexanderius <alexander.krylkov@gmail.com>
2014-07-06 21:54:07 +0600

    XML comments added

--- AcspNet/AppBuilderExtensions.cs
+++ AcspNet/AppBuilderExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace AcspNet
 {
+	/// <summary>
+	/// OWIN IAppBuilder AcspNet extensions
+	/// </summary>
 	public static class AppBuilderExtensions
 	{
+		/// <summary>
+		/// Adds AcspNet in OWIN pipeline
+		/// </summary>
+		/// <param name="builder">The OWIN builder.</param>
+		/// <returns></returns>
 		public static IAppBuilder UseAcspNet(this IAppBuilder builder/* options = null*/)
 		{
 			return builder.Use(null);
