0c2946004afd97df5241fc3e751b5a9c07be7028
Alexanderius <alexander.krylkov@gmail.com>
2014-07-08 22:32:34 +0600

    AcspTypesFinder initial added

---
+++ AcspNet/Meta/AcspTypesFinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AcspNet.Meta
+{
+	/// <summary>
+	/// Provides AcspNet types finder among all loaded assemblies
+	/// </summary>
+	public static class AcspTypesFinder
+	{
+		private static IList<string> _excludedAssembliesPrefixes = new List<string> { "mscorlib", "System", "Microsoft", "AspNet", "AcspNet", "DotNet", "Simplify" };
+
+		/// <summary>
+		/// Gets or sets the excluded assemblies prefixes.
+		/// </summary>
+		/// <value>
+		/// The excluded assemblies prefixes.
+		/// </value>
+		/// <exception cref="System.ArgumentNullException">value</exception>
+		public static IList<string> ExcludedAssembliesPrefixes
+		{
+			get { return _excludedAssembliesPrefixes; }
+			set
+			{
+				if (value == null)
+					throw new ArgumentNullException("value");
+
+				_excludedAssembliesPrefixes = value;
+			}
+		}
+
+		public static Type FindTypeDerivedFrom<T>()
+		{
+			//var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			//var types = GetAssembliesTypes(assemblies);
+
+			return null;
+		}
+		private static ICollection<Type> GetAssembliesTypes(IEnumerable<Assembly> assemblies)
+		{
+			var types = new List<Type>();
+
+			foreach (var assembly in assemblies.Where(assembly => !ExcludedAssembliesPrefixes.Any(prefix => assembly.FullName.StartsWith(prefix))))
+				types.AddRange(assembly.GetTypes());
+
+			return types;
+		}
+	}
+}
