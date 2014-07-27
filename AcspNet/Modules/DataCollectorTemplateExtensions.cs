9d058c1a8de8f5da34b904319bdf1297e33b3b18
Alexanderius <alexander.krylkov@gmail.com>
2014-07-27 22:50:15 +0600

    Data collecotr interface added

---
+++ AcspNet/Modules/DataCollectorTemplateExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Simplify.Templates;
+
+namespace AcspNet.Modules
+{
+	public static class DataCollectorTemplateExtensions
+	{
+		/// <summary>
+		/// Set template main content variable value with data from template (all occurrences will be replaced)
+		/// </summary>
+		/// <param name="dataCollector">The data collector.</param>
+		/// <param name="template">Template to set</param>
+		/// <exception cref="System.NotImplementedException"></exception>
+		public static void Add(this IDataCollector dataCollector, ITemplate template)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
