74786173a8d348bd7d5ac597fedb06d57d63b20d
Alexanderius <alexander.krylkov@gmail.com>
2014-07-27 23:17:33 +0600

    DataCollector added

--- AcspNet/Controller.cs
+++ AcspNet/Controller.cs
@@ -1,10 +1,17 @@
-﻿namespace AcspNet
+﻿using AcspNet.Modules;
+
+namespace AcspNet
 {
 	/// <summary>
 	/// AcspNet controller base class
 	/// </summary>
 	public class Controller : IController
 	{
+		/// <summary>
+		/// Web-site master page data collector.
+		/// </summary>
+		public virtual IDataCollector DataCollector { get; internal set; }
+
 		/// <summary>
 		/// Invokes the controller.
 		/// </summary>
