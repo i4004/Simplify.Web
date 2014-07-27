80aa8b691489118c249a39a3174a201a0a123491
Alexanderius <alexander.krylkov@gmail.com>
2014-07-27 23:15:19 +0600

    IControllerResponseProcessor added

---
+++ AcspNet/IControllerResponseProcessor.cs
@@ -0,0 +1,14 @@
+﻿namespace AcspNet
+{
+	/// <summary>
+	/// Represent controller response processor
+	/// </summary>
+	public interface IControllerResponseProcessor
+	{
+		/// <summary>
+		/// Processes the specified response.
+		/// </summary>
+		/// <param name="response">The response.</param>
+		void Process(IControllerResponse response);
+	}
+}
