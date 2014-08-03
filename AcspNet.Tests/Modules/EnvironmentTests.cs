0c2b4f0ced8de4b03d47db8221a349a58f3f89dd
Alexanderius <alexander.krylkov@gmail.com>
2014-08-03 23:08:17 +0600

    Environment added

---
+++ AcspNet.Tests/Modules/EnvironmentTests.cs
@@ -0,0 +1,31 @@
+﻿using AcspNet.Modules;
+using NUnit.Framework;
+
+namespace AcspNet.Tests.Modules
+{
+	[TestFixture]
+	public class EnvironmentTests
+	{
+		[Test]
+		public void Constructor_DefaultParamenters_PropertiesSetCorrecly()
+		{
+			// Assign
+			var settings = new AcspNetSettings();
+
+			// Act
+			var env = new Environment("C:/Test", settings);
+
+			// Assert
+
+			Assert.AreEqual("Main", env.SiteStyle);
+			Assert.AreEqual("Templates", env.TemplatesPath);
+			Assert.AreEqual("App_Data", env.DataPath);
+			Assert.AreEqual("MainContent", env.MainContentVariableName);
+			Assert.AreEqual("Index.tpl", env.MasterTemplateFileName);
+			Assert.IsFalse(env.TemplatesMemoryCache);
+			Assert.AreEqual("Templates", env.TemplatesPath);
+			Assert.AreEqual("C:/Test/Templates/", env.TemplatesPhysicalPath);
+			Assert.AreEqual("Title", env.TitleVariableName);
+		}
+	}
+}
