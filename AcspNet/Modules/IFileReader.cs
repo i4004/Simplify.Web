5cad15ed5e9a97de774321eb930e102ac4a9ae37
Alexanderius <alexander.krylkov@gmail.com>
2014-08-06 15:33:10 +0600

    File reader initial specification

---
+++ AcspNet/Modules/IFileReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace AcspNet.Modules
+{
+	/// <summary>
+	/// Represents localizable files reader
+	/// </summary>
+	public interface IFileReader : IHideObjectMembers
+	{
+		/// <summary>
+		/// Load xml document from a extension data file
+		/// </summary>
+		/// <param name="extensionsDataFileName">Extension data file name</param>
+		/// <returns>Xml document</returns>
+		XDocument LoadXDocument(string extensionsDataFileName);
+
+		/// <summary>
+		/// Load text from a extension data file
+		/// </summary>
+		/// <param name="extensionsDataFileName">Extension data file name</param>
+		/// <returns>Text from a extension data file</returns>
+		string LoadTextDocument(string extensionsDataFileName);
+	}
+}
