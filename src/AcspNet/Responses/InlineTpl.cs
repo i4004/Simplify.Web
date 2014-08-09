a273a50ecdc372d3cd32da8586c734b19b900bb6
Alexanderius <alexander.krylkov@gmail.com>
2014-08-09 15:39:13 +0600

    Fixes #91

---
+++ src/AcspNet/Responses/InlineTpl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Simplify.Templates;
+
+namespace AcspNet.Responses
+{
+	public class InlineTpl : ControllerResponse
+	{
+		private readonly string _dataCollectorVariableName;
+		private readonly string _data;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InlineTpl"/> class.
+		/// </summary>
+		/// <param name="dataCollectorVariableName">Name of the data collector variable.</param>
+		/// <param name="template">The template.</param>
+		public InlineTpl(string dataCollectorVariableName, ITemplate template)
+		{
+			if (string.IsNullOrEmpty(dataCollectorVariableName))
+				throw new ArgumentNullException("dataCollectorVariableName");
+
+			_dataCollectorVariableName = dataCollectorVariableName;
+
+			if (template != null)
+				_data = template.Get();
+		}
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InlineTpl"/> class.
+		/// </summary>
+		/// <param name="dataCollectorVariableName">Name of the data collector variable.</param>
+		/// <param name="data">The data.</param>
+		public InlineTpl(string dataCollectorVariableName, string data)
+		{
+			if (string.IsNullOrEmpty(dataCollectorVariableName))
+				throw new ArgumentNullException("dataCollectorVariableName");
+
+			_dataCollectorVariableName = dataCollectorVariableName;
+			_data = data;
+		}
+
+		public override void Process()
+		{
+			DataCollector.Add(_dataCollectorVariableName, _data);
+		}
+	}
+}
