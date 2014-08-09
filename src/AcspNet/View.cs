19b7b47f18e931e0e875027152d91e7e6031da9e
Alexanderius <alexander.krylkov@gmail.com>
2014-08-09 15:49:05 +0600

    Fixes #96

--- src/AcspNet/View.cs
+++ src/AcspNet/View.cs
@@ -8,6 +8,14 @@ namespace AcspNet
 	public abstract class View : ViewAccessor, IView
 	{
 		/// <summary>
+		/// Gets the current language.
+		/// </summary>
+		/// <value>
+		/// The current language.
+		/// </value>
+		public virtual string Language { get; internal set; }
+
+		/// <summary>
 		/// Text templates loader.
 		/// </summary>
 		public virtual ITemplateFactory TemplateFactory { get; internal set; }
