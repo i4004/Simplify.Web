1286a8cc116bd20a0f646e0cf5b08bd16cbe3456
Alexanderius <alexander.krylkov@gmail.com>
2014-08-09 15:43:09 +0600

    Fixes #93

--- src/AcspNet/Modules/IAcspNetContext.cs
+++ src/AcspNet/Modules/IAcspNetContext.cs
@@ -16,5 +16,10 @@ namespace AcspNet.Modules
 		/// Gets the query string for current HTTP request.
 		/// </summary>
 		IReadableStringCollection Query { get; }
+
+		/// <summary>
+		/// Gets the form data of post HTTP request.
+		/// </summary>
+		IFormCollection Form { get; }
 	}
 }
