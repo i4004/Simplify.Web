80ac8105621e7465dd0b64dedd10ba1b4879044c
Alexanderius <alexander.krylkov@gmail.com>
2014-08-12 11:50:16 +0600

    Message box responses initial added

---
+++ src/AcspNet/Responses/MessageBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AcspNet.Responses
+{
+	/// <summary>
+	/// Provides message box response (generate message box and puts it to data collector)
+	/// </summary>
+	public class MessageBox : ControllerResponse
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MessageBox"/> class.
+		/// </summary>
+		/// <param name="text">The message box text.</param>
+		/// <param name="status">The message box status.</param>
+		/// <param name="messageBoxTitle">The message box title.</param>
+		public MessageBox(string text, MessageBoxStatus status = MessageBoxStatus.Error, string messageBoxTitle = null)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/AcspNet/Responses/MessageBoxInline.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AcspNet.Responses
+{
+	/// <summary>
+	/// Provides inline message box response (generate inline message box and sends it to user only, without site generation)
+	/// </summary>
+	public class MessageBoxInline : ControllerResponse
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MessageBox"/> class.
+		/// </summary>
+		/// <param name="text">The message box text.</param>
+		/// <param name="status">The message box status.</param>
+		public MessageBoxInline(string text, MessageBoxStatus status = MessageBoxStatus.Error)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
