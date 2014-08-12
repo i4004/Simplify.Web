5c7e2d69880e52f36c3dd9ce86e730daeaa307f4
Alexanderius <alexander.krylkov@gmail.com>
2014-08-12 11:11:33 +0600

    AcspContext properties added

--- src/AcspNet/Modules/AcspNetContext.cs
+++ src/AcspNet/Modules/AcspNetContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using Microsoft.Owin;
 
 namespace AcspNet.Modules
 {
@@ -7,6 +8,8 @@ namespace AcspNet.Modules
 	/// </summary>
 	public class AcspNetContext : IAcspNetContext
 	{
+		private readonly Lazy<IFormCollection> _form;
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="AcspNetContext"/> class.
 		/// </summary>
@@ -14,11 +17,44 @@ namespace AcspNet.Modules
 		public AcspNetContext(IOwinContext context)
 		{
 			Context = context;
+			Request = context.Request;
+			Response = context.Response;
+			Query = context.Request.Query;
+
+			_form = new Lazy<IFormCollection>(() =>
+			{
+				var task = context.Request.ReadFormAsync();
+				task.Wait();
+				return task.Result;
+			});
 		}
 
 		/// <summary>
 		/// Gets the context for the current HTTP request.
 		/// </summary>
 		public IOwinContext Context { get; private set; }
+
+		/// <summary>
+		/// Gets the request for the current HTTP request.
+		/// </summary>
+		public IOwinRequest Request { get; private set; }
+
+		/// <summary>
+		/// Gets the response for the current HTTP request.
+		/// </summary>
+		public IOwinResponse Response { get; private set; }
+
+		/// <summary>
+		/// Gets the query string for current HTTP request.
+		/// </summary>
+		public IReadableStringCollection Query { get; private set; }
+
+		/// <summary>
+		/// Gets the form data of post HTTP request.
+		/// </summary>
+		public IFormCollection Form
+		{
+			get { return _form.Value; }
+		}
 	}
 }
--- src/AcspNet/Modules/IAcspNetContext.cs
+++ src/AcspNet/Modules/IAcspNetContext.cs
@@ -11,5 +11,25 @@ namespace AcspNet.Modules
 		/// Gets the context for the current HTTP request.
 		/// </summary>
 		IOwinContext Context { get; }
+
+		/// <summary>
+		/// Gets the request for the current HTTP request.
+		/// </summary>
+		IOwinRequest Request { get; }
+
+		/// <summary>
+		/// Gets the response for the current HTTP request.
+		/// </summary>
+		IOwinResponse Response { get; }
+		
+		/// <summary>
+		/// Gets the query string for current HTTP request.
+		/// </summary>
+		IReadableStringCollection Query { get; }
+
+		/// <summary>
+		/// Gets the form data of post HTTP request.
+		/// </summary>
+		IFormCollection Form { get; }
 	}
 }
