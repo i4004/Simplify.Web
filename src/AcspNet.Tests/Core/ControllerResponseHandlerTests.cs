dd757d9234fdde296258ee8211adc2e0a7cc8ad7
Alexanderius <alexander.krylkov@gmail.com>
2014-08-15 16:47:15 +0600

    Initial

---
+++ src/AcspNet.Tests/Core/ControllerResponseHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using AcspNet.Core;
+using Moq;
+using NUnit.Framework;
+
+namespace AcspNet.Tests.Core
+{
+	[TestFixture]
+	public class ControllerResponseHandlerTests
+	{
+		private Mock<IControllerResponseBuilder> _controllerResponseBuilder;
+		private ControllerResponseHandler _controllerResponseHandler;
+
+		[SetUp]
+		public void Initialize()
+		{
+			_controllerResponseBuilder = new Mock<IControllerResponseBuilder>();
+			_controllerResponseHandler = new ControllerResponseHandler();
+		}
+
+		[Test]
+		public void Test()
+		{
+			// Assign
+	
+			// Act
+
+			// Assert
+		}
+	}
+}
