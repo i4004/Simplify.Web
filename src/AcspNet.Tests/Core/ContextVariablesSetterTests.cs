1612b9380d63cb464b88dfdd2cc3eadfb75ffc9e
Alexanderius <alexander.krylkov@gmail.com>
2014-08-17 21:53:06 +0600

    Fixes #120

---
+++ src/AcspNet.Tests/Core/ContextVariablesSetterTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using AcspNet.Core;
+using AcspNet.Modules;
+using Microsoft.Owin;
+using Moq;
+using NUnit.Framework;
+using Simplify.DI;
+
+namespace AcspNet.Tests.Core
+{
+	[TestFixture]
+	public class ContextVariablesSetterTests
+	{
+		private Mock<IDataCollector> _dataCollector;
+		private ContextVariablesSetter _setter;
+		private Mock<IDIContainerProvider> _containerProvider;
+
+		private Mock<IEnvironment> _environment;
+		private Mock<ILanguageManagerProvider> _languageManagerProvider;
+		private Mock<ILanguageManager> _languageManager;
+		private Mock<IAcspNetContextProvider> _contextProvider;
+		private Mock<IAcspNetContext> _context;
+		private Mock<IStopwatchProvider> _stopwatchProvider;
+
+		[SetUp]
+		public void Initialize()
+		{
+			_dataCollector = new Mock<IDataCollector>();
+			_setter = new ContextVariablesSetter(_dataCollector.Object);
+			_containerProvider = new Mock<IDIContainerProvider>();
+
+			_environment = new Mock<IEnvironment>();
+			_languageManagerProvider = new Mock<ILanguageManagerProvider>();
+			_languageManager = new Mock<ILanguageManager>();
+			_contextProvider = new Mock<IAcspNetContextProvider>();
+			_context = new Mock<IAcspNetContext>();
+			_stopwatchProvider = new Mock<IStopwatchProvider>();
+
+			_environment.SetupGet(x => x.TemplatesPath).Returns("Templates");
+			_environment.SetupGet(x => x.SiteStyle).Returns("Main");
+
+			_languageManager.SetupGet(x => x.Language).Returns("ru");
+			_languageManagerProvider.Setup(x => x.Get()).Returns(_languageManager.Object);
+
+			_context.SetupGet(x => x.SiteUrl).Returns("http://localhost/mysite/");
+			_contextProvider.Setup(x => x.Get()).Returns(_context.Object);
+			_context.SetupGet(x => x.Request.PathBase).Returns(new PathString("/mysite"));
+
+			_stopwatchProvider.Setup(x => x.StopAndGetMeasurement()).Returns(new TimeSpan(0, 0, 1, 15, 342));
+
+			_containerProvider.Setup(x => x.Resolve(It.Is<Type>(d => d == typeof(IEnvironment)))).Returns(_environment.Object);
+			_containerProvider.Setup(x => x.Resolve(It.Is<Type>(d => d == typeof(ILanguageManagerProvider)))).Returns(_languageManagerProvider.Object);
+			_containerProvider.Setup(x => x.Resolve(It.Is<Type>(d => d == typeof(IAcspNetContextProvider)))).Returns(_contextProvider.Object);
+			_containerProvider.Setup(x => x.Resolve(It.Is<Type>(d => d == typeof(IStopwatchProvider)))).Returns(_stopwatchProvider.Object);
+		}
+
+		[Test]
+		public void SetVariables_NormalData_AllVariablesSetToDataCollector()
+		{
+			// Act
+			_setter.SetVariables(_containerProvider.Object);
+
+			// Assert
+
+			_dataCollector.Verify(x => x.Add(It.Is<string>(d => d == ContextVariablesSetter.VariableNameTemplatesPath), It.Is<string>(d => d == "Templates")));
+			_dataCollector.Verify(x => x.Add(It.Is<string>(d => d == ContextVariablesSetter.VariableNameSiteStyle), It.Is<string>(d => d == "Main")));
+			_dataCollector.Verify(x => x.Add(It.Is<string>(d => d == ContextVariablesSetter.VariableNameCurrentLanguage), It.Is<string>(d => d == "ru")));
+			_dataCollector.Verify(x => x.Add(It.Is<string>(d => d == ContextVariablesSetter.VariableNameCurrentLanguageExtension), It.Is<string>(d => d == ".ru")));
+			_dataCollector.Verify(x => x.Add(It.Is<string>(d => d == ContextVariablesSetter.VariableNameSiteUrl), It.Is<string>(d => d == "http://localhost/mysite/")));
+			_dataCollector.Verify(x => x.Add(It.Is<string>(d => d == ContextVariablesSetter.VariableNameSiteVirtualPath), It.Is<string>(d => d == "/mysite")));
+			_dataCollector.Verify(x => x.Add(It.Is<string>(d => d == ContextVariablesSetter.VariableNameExecutionTime), It.Is<string>(d => d == "01:15:342")));
+		}
+	}
+}
--- src/AcspNet/Core/ContextVariablesSetter.cs
+++ src/AcspNet/Core/ContextVariablesSetter.cs
@@ -8,10 +8,6 @@ namespace AcspNet.Core
 	/// </summary>
 	public class ContextVariablesSetter : IContextVariablesSetter
 	{
-		/// <summary>
-		/// Site generation time templates variable name
-		/// </summary>
-		public const string VariableNameSiteExecutionTime = "SV:SiteExecutionTime";
 
 		/// <summary>
 		/// The site variable name templates dir
@@ -21,27 +17,40 @@ namespace AcspNet.Core
 		/// <summary>
 		/// The site variable name current style
 		/// </summary>
-		public const string VariableNameCurrentStyle = "SV:Style";
+		public const string VariableNameSiteStyle = "SV:Style";
 
 		/// <summary>
 		/// The site variable name current language
 		/// </summary>
 		public const string VariableNameCurrentLanguage = "SV:Language";
+
 		/// <summary>
 		/// The site variable name current language extension
 		/// </summary>
 		public const string VariableNameCurrentLanguageExtension = "SV:LanguageExt";
+
 		/// <summary>
 		/// The site variable name site URL
 		/// </summary>
 		public const string VariableNameSiteUrl = "SV:SiteUrl";
+
 		/// <summary>
 		/// The site variable name site virtual path (returns '/yoursite' if your site is placed in virtual directory like http://yourdomain.com/yoursite/)
 		/// </summary>
 		public const string VariableNameSiteVirtualPath = "~";
 
+		/// <summary>
+		/// Site generation time templates variable name
+		/// </summary>
+		public const string VariableNameExecutionTime = "SV:SiteExecutionTime";
+
+
 		private readonly IDataCollector _dataCollector;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ContextVariablesSetter"/> class.
+		/// </summary>
+		/// <param name="dataCollector">The data collector.</param>
 		public ContextVariablesSetter(IDataCollector dataCollector)
 		{
 			_dataCollector = dataCollector;
@@ -53,7 +62,21 @@ namespace AcspNet.Core
 		/// <param name="containerProvider">The DI container provider.</param>
 		public void SetVariables(IDIContainerProvider containerProvider)
 		{
-			throw new System.NotImplementedException();
+			var environment = containerProvider.Resolve<IEnvironment>();
+			var languageManager = containerProvider.Resolve<ILanguageManagerProvider>().Get();
+			var context = containerProvider.Resolve<IAcspNetContextProvider>().Get();
+			var stopWatchProvider = containerProvider.Resolve<IStopwatchProvider>();
+
+			_dataCollector.Add(VariableNameTemplatesPath, environment.TemplatesPath);
+			_dataCollector.Add(VariableNameSiteStyle, environment.SiteStyle);
+
+			_dataCollector.Add(VariableNameCurrentLanguage, languageManager.Language);
+			_dataCollector.Add(VariableNameCurrentLanguageExtension, !string.IsNullOrEmpty(languageManager.Language) ? "." + languageManager.Language : "");
+
+			_dataCollector.Add(VariableNameSiteUrl, context.SiteUrl);
+			_dataCollector.Add(VariableNameSiteVirtualPath, context.Request.PathBase.Value);
+
+			_dataCollector.Add(VariableNameExecutionTime, stopWatchProvider.StopAndGetMeasurement().ToString("mm\\:ss\\:fff"));
 		}
 	}
 }
