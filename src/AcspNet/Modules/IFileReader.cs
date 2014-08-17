6d944583b16a58e383334036048f15ccf407e65c
Alexanderius <alexander.krylkov@gmail.com>
2014-08-17 12:36:03 +0600

    Language-specific methods added

--- src/AcspNet/Modules/IFileReader.cs
+++ src/AcspNet/Modules/IFileReader.cs
@@ -8,17 +8,33 @@ namespace AcspNet.Modules
 	public interface IFileReader : IHideObjectMembers
 	{
 		/// <summary>
-		/// Load xml document from a extension data file
+		/// Load xml document from a file
 		/// </summary>
-		/// <param name="extensionsDataFileName">Extension data file name</param>
+		/// <param name="fileName">File name</param>
 		/// <returns>Xml document</returns>
-		XDocument LoadXDocument(string extensionsDataFileName);
+		XDocument LoadXDocument(string fileName);
+		
+		/// <summary>
+		/// Load xml document from a file with specific language
+		/// </summary>
+		/// <param name="fileName">File name</param>
+		/// <param name="language">File language</param>
+		/// <returns>Xml document</returns>
+		XDocument LoadXDocument(string fileName, string language);
 
 		/// <summary>
 		/// Load text from a extension data file
 		/// </summary>
-		/// <param name="extensionsDataFileName">Extension data file name</param>
-		/// <returns>Text from a extension data file</returns>
-		string LoadTextDocument(string extensionsDataFileName);
+		/// <param name="fileName">File name</param>
+		/// <returns>Text from a file</returns>
+		string LoadTextDocument(string fileName);
+		
+		/// <summary>
+		/// Load text from a file with specific language
+		/// </summary>
+		/// <param name="fileName">File name</param>
+		/// <param name="language">File language</param>
+		/// <returns>Text from a file</returns>
+		string LoadTextDocument(string fileName, string language);
 	}
 }
