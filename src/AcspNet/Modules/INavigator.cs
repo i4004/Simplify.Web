7a6c17cbf9b1dc1414d40c955ba0003a55485e94
Alexanderius <alexander.krylkov@gmail.com>
2014-08-19 21:59:09 +0600

    Navigator initial added

---
+++ src/AcspNet/Modules/INavigator.cs
@@ -0,0 +1,51 @@
+﻿using AcspNet.Responses;
+
+namespace AcspNet.Modules
+{
+	/// <summary>
+	/// Represent website navigation manager, which controls current user location, link to previous page and link to specified page
+	/// </summary>
+	public interface INavigator : IHideObjectMembers
+	{
+		/// <summary>
+		/// Gets or sets the previous page link.
+		/// </summary>
+		/// <value>
+		/// The previous page link.
+		/// </value>
+		string PreviousPageLink { get; set; }
+
+		/// <summary>
+		/// Gets or sets the redirect link.
+		/// </summary>
+		/// <value>
+		/// The redirect link.
+		/// </value>
+		string RedirectLink { get; set; }
+
+		/// <summary>
+		/// Gets or sets the previous navigated URL.
+		/// </summary>
+		/// <value>
+		/// The previous navigated URL.
+		/// </value>
+		string PreviousNavigatedUrl { get; set; }
+
+		/// <summary>
+		/// Sets the redirect link to current page.
+		/// </summary>
+		void SetRedirectLinkToCurrentPage();
+
+		/// <summary>
+		/// Navigates the client by specified navigation type.
+		/// </summary>
+		/// <param name="navigationType">Type of the navigation.</param>
+		void Navigate(NavigationType navigationType);
+
+		/// <summary>
+		/// Redirects the client to specified URL.
+		/// </summary>
+		/// <param name="url">The URL.</param>
+		void Redirect(string url);
+	}
+}
---
+++ src/AcspNet/Modules/Navigator.cs
@@ -0,0 +1,63 @@
+﻿using AcspNet.Responses;
+
+namespace AcspNet.Modules
+{
+	/// <summary>
+	/// Provides website navigation manager, which controls current user location, link to previous page and link to specified page
+	/// </summary>
+	public class Navigator : INavigator
+	{
+		/// <summary>
+		/// Gets or sets the previous page link.
+		/// </summary>
+		/// <value>
+		/// The previous page link.
+		/// </value>
+		public string PreviousPageLink { get; set; }
+
+		/// <summary>
+		/// Gets or sets the redirect link.
+		/// </summary>
+		/// <value>
+		/// The redirect link.
+		/// </value>
+		public string RedirectLink { get; set; }
+
+		/// <summary>
+		/// Gets or sets the previous navigated URL.
+		/// </summary>
+		/// <value>
+		/// The previous navigated URL.
+		/// </value>
+		public string PreviousNavigatedUrl { get; set; }
+
+		/// <summary>
+		/// Sets the redirect link to current page.
+		/// </summary>
+		/// <exception cref="System.NotImplementedException"></exception>
+		public void SetRedirectLinkToCurrentPage()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Navigates the client by specified navigation type.
+		/// </summary>
+		/// <param name="navigationType">Type of the navigation.</param>
+		/// <exception cref="System.NotImplementedException"></exception>
+		public void Navigate(NavigationType navigationType)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Redirects the client to specified URL.
+		/// </summary>
+		/// <param name="url">The URL.</param>
+		/// <exception cref="System.NotImplementedException"></exception>
+		public void Redirect(string url)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
