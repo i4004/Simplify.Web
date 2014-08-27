64de7bfc4818d30fb08c2190b0872e16eef209f2
Alexanderius <alexander.krylkov@gmail.com>
2014-08-27 11:15:34 +0600

    Request handler added

---
+++ src/AcspNet/Core/IRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+using Simplify.DI;
+
+namespace AcspNet.Core
+{
+	/// <summary>
+	/// Represent OWIN HTTP request handler
+	/// </summary>
+	public interface IRequestHandler
+	{
+		/// <summary>
+		/// Processes the OWIN HTTP request.
+		/// </summary>
+		/// <param name="containerProvider">The DI container provider.</param>
+		/// <param name="context">The context.</param>
+		/// <returns></returns>
+		Task ProcessRequest(IDIContainerProvider containerProvider, IOwinContext context);
+	}
+}
---
+++ src/AcspNet/Core/RequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+using Simplify.DI;
+
+namespace AcspNet.Core
+{
+	/// <summary>
+	/// Provides OWIN HTTP request handler
+	/// </summary>
+	public class RequestHandler : IRequestHandler
+	{
+		private readonly IControllersRequestHandler _controllersRequestHandler;
+
+		public RequestHandler(IControllersRequestHandler controllersRequestHandler)
+		{
+			_controllersRequestHandler = controllersRequestHandler;
+		}
+
+		/// <summary>
+		/// Processes the OWIN HTTP request.
+		/// </summary>
+		/// <param name="containerProvider">The DI container provider.</param>
+		/// <param name="context">The context.</param>
+		/// <returns></returns>
+		public Task ProcessRequest(IDIContainerProvider containerProvider, IOwinContext context)
+		{
+			return _controllersRequestHandler.ProcessRequest(containerProvider, context);
+		}
+	}
+}
