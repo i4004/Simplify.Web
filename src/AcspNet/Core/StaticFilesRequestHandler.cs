41d21776760396d8bcbe8531ddd01fbc07135095
Alexanderius <alexander.krylkov@gmail.com>
2014-08-27 15:37:46 +0600

    Using fixed

--- src/AcspNet/Core/StaticFilesRequestHandler.cs
+++ src/AcspNet/Core/StaticFilesRequestHandler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
 using System.Threading.Tasks;
-using AcspNet.Responses;
 using Microsoft.Owin;
 
 namespace AcspNet.Core
