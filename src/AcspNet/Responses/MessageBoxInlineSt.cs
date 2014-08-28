afc1c963e8e3b00b7c33b9632e7770e128bf9579
Alexanderius <alexander.krylkov@gmail.com>
2014-08-28 23:15:54 +0600

    Fixes #131

---
+++ src/AcspNet/Responses/MessageBoxInlineSt.cs
@@ -0,0 +1,35 @@
+﻿using AcspNet.Modules.Html;
+
+namespace AcspNet.Responses
+{
+	/// <summary>
+	/// Provides inline message box response (generate inline message box and sends it to user only, without site generation)
+	/// </summary>
+	public class MessageBoxInlineSt : ControllerResponse
+	{
+		private readonly string _stringTableItemName;
+		private readonly MessageBoxStatus _status;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MessageBox" /> class.
+		/// </summary>
+		/// <param name="stringTableItemName">Name of the string table item.</param>
+		/// <param name="status">The message box status.</param>
+		public MessageBoxInlineSt(string stringTableItemName, MessageBoxStatus status = MessageBoxStatus.Error)
+		{
+			_stringTableItemName = stringTableItemName;
+			_status = status;
+		}
+
+		/// <summary>
+		/// Processes this response
+		/// </summary>
+		/// <returns></returns>
+		public override ControllerResponseResult Process()
+		{
+			ResponseWriter.Write(Html.MessageBox.GetInlineSt(_stringTableItemName, _status), Context.Response);
+
+			return ControllerResponseResult.RawOutput;
+		}
+	}
+}
