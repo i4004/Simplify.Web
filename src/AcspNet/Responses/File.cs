6c9fbed6cfa9e9ed09251f29462ca077e3b7832a
Alexanderius <alexander.krylkov@gmail.com>
2014-08-28 21:25:41 +0600

    Response as file implemented

--- src/AcspNet/Responses/File.cs
+++ src/AcspNet/Responses/File.cs
@@ -7,24 +7,37 @@ namespace AcspNet.Responses
 	/// </summary>
 	public class File : ControllerResponse
 	{
+		private readonly string _outputFileName;
+		private readonly string _contentType;
+		private readonly byte[] _data;
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="File"/> class.
 		/// </summary>
+		/// <param name="outputFileName">The name of the file.</param>
 		/// <param name="contentType">Type of the content.</param>
-		/// <param name="name">The name of the file.</param>
 		/// <param name="data">The data of the file.</param>
-		public File(string contentType, string name, byte[] data)
+		public File(string outputFileName, string contentType, byte[] data)
 		{
-			throw new NotImplementedException();
+			if (outputFileName == null) throw new ArgumentNullException("outputFileName");
+			if (contentType == null) throw new ArgumentNullException("contentType");
+			if (data == null) throw new ArgumentNullException("data");
+
+			_outputFileName = outputFileName;
+			_contentType = contentType;
+			_data = data;
 		}
 
 		/// <summary>
 		/// Processes this response
 		/// </summary>
-		/// <exception cref="System.NotImplementedException"></exception>
 		public override ControllerResponseResult Process()
 		{
-			throw new NotImplementedException();
+			Context.Response.Headers.Append("Content-Disposition", "attachment; filename=" + _outputFileName);
+			Context.Response.ContentType = _contentType;
+			Context.Response.Write(_data);
+
+			return ControllerResponseResult.RawOutput;
 		}
 	}
 }
