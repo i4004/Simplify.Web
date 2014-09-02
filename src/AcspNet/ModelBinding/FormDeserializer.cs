60fa7b5a8a821d07e5daf6c831bc3268640e796b
Alexanderius <alexander.krylkov@gmail.com>
2014-09-02 19:30:43 +0600

    Implemented basic form binding

--- src/AcspNet/ModelBinding/FormDeserializer.cs
+++ src/AcspNet/ModelBinding/FormDeserializer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Owin;
 
 namespace AcspNet.ModelBinding
@@ -9,37 +11,65 @@ namespace AcspNet.ModelBinding
 	/// </summary>
 	public static class FormDeserializer
 	{
+		private static readonly Type RequiredAttributeType = typeof(RequiredAttribute);
+
 		/// <summary>
 		/// Deserializes this instance.
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <returns></returns>
-		/// <exception cref="System.NotImplementedException"></exception>
 		public static T Deserialize<T>(IFormCollection form)
 		{
-			var type = typeof (T);
-			var requiredAttributeType = typeof(RequiredAttribute);
+			var type = typeof(T);
 
 			var obj = Activator.CreateInstance<T>();
 
 			foreach (var propInfo in type.GetProperties())
-			{
-				var propertyInfo = propInfo;
-				var isRequired = propInfo.CustomAttributes.Any(x => x.AttributeType == requiredAttributeType);
-
-				var formField = form.FirstOrDefault(x => x.Key == propertyInfo.Name);
-
-				var value = !string.IsNullOrEmpty(formField.Value[0]) ? formField.Value[0] : null;
-
-				ParseProperty(isRequired, value);
-			}
+				propInfo.SetValue(obj, ParseProperty(propInfo, form));
 
 			return obj;
 		}
 
-		private static object ParseProperty(bool isRequired, string value)
-		{			
-			throw new NotImplementedException();
+		private static object ParseProperty(PropertyInfo propertyInfo, IEnumerable<KeyValuePair<string, string[]>> form)
+		{
+			var isRequired = propertyInfo.CustomAttributes.Any(x => x.AttributeType == RequiredAttributeType);
+			var formField = form.FirstOrDefault(x => x.Key == propertyInfo.Name);
+
+			string rawValue = null;
+
+			if (!formField.Equals(default(KeyValuePair<string, string[]>)) && !string.IsNullOrEmpty(formField.Value[0]))
+				rawValue = formField.Value[0];
+
+			if (isRequired && string.IsNullOrEmpty(rawValue))
+				throw new ModelBindingException(string.Format("Required parameter '{0}' is null or empty", propertyInfo.Name));
+
+			var parsedValue = ParseValue(rawValue, propertyInfo.PropertyType);
+
+			if (parsedValue == null && isRequired)
+				throw new ModelBindingException(string.Format("Required property type is: '{0}', actual value: '{1}'", propertyInfo.PropertyType.ToString(), rawValue));
+
+			return parsedValue;
+		}
+
+		private static object ParseValue(string value, Type requiredDataType)
+		{
+			if (requiredDataType == typeof(string))
+				return value;
+
+			if (requiredDataType == typeof(int))
+			{
+				int buffer;
+
+				if (int.TryParse(value, out buffer))
+					return null;
+
+				return buffer;
+			}
+
+			if (requiredDataType == typeof(bool))
+				return value == "on";
+
+			return value;
 		}
 	}
 }
---
+++ src/AcspNet/ModelBinding/ModelBindingException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AcspNet.ModelBinding
+{
+	/// <summary>
+	/// Represent model binding exceptions
+	/// </summary>
+	public class ModelBindingException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ModelBindingException"/> class.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public ModelBindingException(string message) : base(message)
+		{
+		}
+	}
+}
