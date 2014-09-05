b58a4f8c9ab8fa312cdc7d6d4720a3ca8e41083e
Alexanderius <alexander.krylkov@gmail.com>
2014-09-05 14:51:35 +0600

    Fixes #173

---
+++ src/Examples/AcspNet.Examples.SelfHosted.Tests/Controllers/Accounts/LogoutControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using AcspNet.Examples.SelfHosted.Controllers.Accounts;
+using AcspNet.Modules;
+using AcspNet.Responses;
+using Moq;
+using NUnit.Framework;
+
+namespace AcspNet.Examples.SelfHosted.Tests.Controllers.Accounts
+{
+	[TestFixture]
+	public class LogoutControllerTests
+	{
+		[Test]
+		public void Invoke_SignOutCalledRedirectedToDefaultPage()
+		{
+			// Assign
+
+			var c = new Mock<LogoutController> {CallBase = true};
+			var context = new Mock<IAcspNetContext>();
+			context.Setup(x => x.Context.Authentication.SignOut());
+			c.SetupGet(x => x.Context).Returns(context.Object);
+
+			// Act
+			var result = c.Object.Invoke();
+
+			// Assert
+
+			Assert.AreEqual(RedirectionType.DefaultPage, ((Redirect)result).RedirectionType);
+			context.Verify(x => x.Context.Authentication.SignOut());
+		}
+	}
+}
