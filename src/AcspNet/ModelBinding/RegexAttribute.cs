61e6453d713aaf569b2053d92f4b366ce54cda93
Alexanderius <alexander.krylkov@gmail.com>
2014-09-08 10:12:59 +0600

    Regex attribute added

---
+++ src/AcspNet/ModelBinding/RegexAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AcspNet.ModelBinding
+{
+	/// <summary>
+	/// Indicates what this property should match regular expression
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Property)]
+	public class RegexAttribute : Attribute
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RegexAttribute"/> class.
+		/// </summary>
+		/// <param name="regexString">The regex string.</param>
+		public RegexAttribute(string regexString)
+		{
+			RegexString = regexString;
+		}
+
+		/// <summary>
+		/// Gets or sets the regex string.
+		/// </summary>
+		/// <value>
+		/// The regex string.
+		/// </value>
+		public string RegexString { get; set; }
+	}
+}
