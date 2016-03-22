02269b5b67c95d19887ec6c6ef662e3e5665a547
Alexanderius <alexander.krylkov@gmail.com>
2016-03-22 12:19:56 +0600

    PatchAttribute added

---
+++ src/Simplify.Web/Attributes/PatchAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Simplify.Web.Attributes
+{
+	/// <summary>
+	/// Set controller HTTP PATCH request route path
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public class PatchAttribute : Attribute
+	{
+		/// <summary>
+		/// Gets the route.
+		/// </summary>
+		/// <value>
+		/// The route.
+		/// </value>
+		public string Route { get; private set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PutAttribute"/> class.
+		/// </summary>
+		/// <param name="route">The route.</param>
+		public PatchAttribute(string route)
+		{
+			Route = route;
+		}
+	}
+}
