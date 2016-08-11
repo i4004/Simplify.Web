4471d71cb6ad060a7fdc49d6fe98223143cfb8ed
Alexanderius <alexander.krylkov@gmail.com>
2016-08-11 19:56:05 +0600

    Refactored to new C# spec

--- src/Simplify.Web/ModelBinding/Validation/StringValidator.cs
+++ src/Simplify.Web/ModelBinding/Validation/StringValidator.cs
@@ -25,7 +25,8 @@ namespace Simplify.Web.ModelBinding.Validation
 			{
 				var minLength = ((MinLengthAttribute)attributes[0]).MinimumPropertyLength;
 				if (string.IsNullOrEmpty(value) || value.Length < minLength)
-					throw new ModelValidationException(string.Format("Property '{0}' required minimum length is '{1}', actual value: '{2}'", propertyInfo.Name, minLength, value));
+					throw new ModelValidationException(
+						$"Property '{propertyInfo.Name}' required minimum length is '{minLength}', actual value: '{value}'");
 			}
 
 			attributes = propertyInfo.GetCustomAttributes(typeof(MaxLengthAttribute), false);
@@ -34,7 +35,8 @@ namespace Simplify.Web.ModelBinding.Validation
 			{
 				var maxLength = ((MaxLengthAttribute)attributes[0]).MaximumPropertyLength;
 				if (string.IsNullOrEmpty(value) || value.Length > maxLength)
-					throw new ModelValidationException(string.Format("Property '{0}' required maximum length is '{1}', actual value: '{2}'", propertyInfo.Name, maxLength, value));
+					throw new ModelValidationException(
+						$"Property '{propertyInfo.Name}' required maximum length is '{maxLength}', actual value: '{value}'");
 			}
 
 			attributes = propertyInfo.GetCustomAttributes(typeof(EMailAttribute), false);
@@ -42,7 +44,7 @@ namespace Simplify.Web.ModelBinding.Validation
 			if (attributes.Length > 0)
 			{
 				if (!StringHelper.ValidateEMail(value))
-					throw new ModelValidationException(string.Format("Property '{0}' should be an email, actual value: '{1}'", propertyInfo.Name, value));
+					throw new ModelValidationException($"Property '{propertyInfo.Name}' should be an email, actual value: '{value}'");
 			}
 
 			attributes = propertyInfo.GetCustomAttributes(typeof(RegexAttribute), false);
@@ -52,7 +54,8 @@ namespace Simplify.Web.ModelBinding.Validation
 				var regexString = ((RegexAttribute)attributes[0]).RegexString;
 
 				if (string.IsNullOrEmpty(value) || !Regex.IsMatch(value, regexString))
-					throw new ModelValidationException(string.Format("Property '{0}' regex not matched, actual value: '{1}', pattern: '{2}'", propertyInfo.Name, value, regexString));
+					throw new ModelValidationException(
+						$"Property '{propertyInfo.Name}' regex not matched, actual value: '{value}', pattern: '{regexString}'");
 			}
 		}
 	}
