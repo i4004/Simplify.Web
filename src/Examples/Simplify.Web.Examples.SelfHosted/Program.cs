cd0db002be4d4d6481bd9e702d3989eccae6f259
Alexanderius <alexander.krylkov@gmail.com>
2016-09-22 21:54:05 +0600

    Access denied fix

--- src/Examples/Simplify.Web.Examples.SelfHosted/Program.cs
+++ src/Examples/Simplify.Web.Examples.SelfHosted/Program.cs
@@ -7,7 +7,7 @@ namespace Simplify.Web.Examples.SelfHosted
 	{
 		private static void Main()
 		{
-			using (WebApp.Start<Startup>("http://+:8080"))
+			using (WebApp.Start<Startup>("http://localhost:8080"))
 			{
 				Console.WriteLine("Running a http server on port 8080");
 				Console.ReadLine();
