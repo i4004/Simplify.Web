08665eb8b7d3bacaa2d570f98572964669fed7e3
Alexanderius <alexander.krylkov@gmail.com>
2017-08-19 19:37:00 +0600

    Tests run configuration upgrade to nunit 3

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -2,13 +2,13 @@ mono --runtime=v4.0 src/.nuget/NuGet.exe install NUnit.Runners -Version 3.7.0 -o
 
 runTest()
 {
-    mono --runtime=v4.0 packages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe -noxml -nodots -labels $@
+    mono --runtime=v4.0 packages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe -labels After $@
    if [ $? -ne 0 ]
    then   
      exit 1
    fi
 }
 
-runTest $1 -exclude:Windows
+runTest $1
 
 exit $?
