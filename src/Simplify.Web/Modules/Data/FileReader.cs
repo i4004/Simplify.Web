7e90bfd54f5dd4cdbac9fbfe45e44d0d1692c82e
Alexanderius <alexander.krylkov@gmail.com>
2017-08-23 22:46:29 +0600

    Fixes #47 + refactoring

--- src/Simplify.Web/Modules/Data/FileReader.cs
+++ src/Simplify.Web/Modules/Data/FileReader.cs
@@ -11,8 +11,12 @@ namespace Simplify.Web.Modules.Data
 	/// </summary>
 	public class FileReader : IFileReader
 	{
-		private static readonly IDictionary<KeyValuePair<string, string>, XDocument> XmlCache = new Dictionary<KeyValuePair<string, string>, XDocument>();
-		private static readonly IDictionary<KeyValuePair<string, string>, string> TextCache = new Dictionary<KeyValuePair<string, string>, string>();
+		private static readonly IDictionary<KeyValuePair<string, string>, XDocument> XmlCache =
+			new Dictionary<KeyValuePair<string, string>, XDocument>();
+
+		private static readonly IDictionary<KeyValuePair<string, string>, string> TextCache =
+			new Dictionary<KeyValuePair<string, string>, string>();
+
 		private static readonly object Locker = new object();
 
 		private readonly string _dataPhysicalPath;
@@ -30,7 +34,8 @@ namespace Simplify.Web.Modules.Data
 		/// <param name="defaultLanguage">The default language.</param>
 		/// <param name="languageManagerProvider">The language manager provider.</param>
 		/// <param name="disableCache">Disable FileReader cache.</param>
-		public FileReader(string dataPhysicalPath, string defaultLanguage, ILanguageManagerProvider languageManagerProvider, bool disableCache = false)
+		public FileReader(string dataPhysicalPath, string defaultLanguage, ILanguageManagerProvider languageManagerProvider,
+			bool disableCache = false)
 		{
 			_dataPhysicalPath = dataPhysicalPath;
 			_defaultLanguage = defaultLanguage;
@@ -53,16 +58,14 @@ namespace Simplify.Web.Modules.Data
 		}
 
 		/// <summary>
-		/// Clears the cache.
+		/// Setups the file reader.
 		/// </summary>
-		public static void ClearCache()
+		public void Setup()
 		{
-			lock (Locker)
-			{
-				XmlCache.Clear();
-				TextCache.Clear();
-			}
+			_languageManager = _languageManagerProvider.Get();
 		}
+
+		#region Paths
 
 		/// <summary>
 		/// Get file path
@@ -98,86 +101,9 @@ namespace Simplify.Web.Modules.Data
 			return $"{_dataPhysicalPath}{fileNameFirstPart}.{language}{fileNameLastPart}";
 		}
 
-		/// <summary>
-		/// Setups the file reader.
-		/// </summary>
-		public void Setup()
-		{
-			_languageManager = _languageManagerProvider.Get();
-		}
+		#endregion Paths
 
-		/// <summary>
-		/// Load xml document from a file located in data folder
-		/// </summary>
-		/// <param name="fileName">File name</param>
-		/// <param name="memoryCache">Load file from memory cache if possible.</param>
-		/// <returns>
-		/// Xml document
-		/// </returns>
-		public XDocument LoadXDocument(string fileName, bool memoryCache = false)
-		{
-			return LoadXDocument(fileName, _languageManager.Language, memoryCache);
-		}
-
-		/// <summary>
-		/// Load xml document from a file with specific language located in data folder
-		/// </summary>
-		/// <param name="fileName">File name</param>
-		/// <param name="language">File language</param>
-		/// <param name="memoryCache">Load file from memory cache if possible.</param>
-		/// <returns>
-		/// Xml document
-		/// </returns>
-		/// <exception cref="ArgumentNullException">fileName</exception>
-		public XDocument LoadXDocument(string fileName, string language, bool memoryCache = false)
-		{
-			if (string.IsNullOrEmpty(fileName)) throw new ArgumentNullException(nameof(fileName));
-
-			if (!fileName.EndsWith(".xml"))
-				fileName = fileName + ".xml";
-
-			if (!memoryCache || _disableCache)
-			{
-				var filePath = GetFilePath(fileName, language);
-
-				if (FileSystem.File.Exists(filePath))
-					return XDocument.Parse(FileSystem.File.ReadAllText(filePath));
-
-				filePath = GetFilePath(fileName, _defaultLanguage);
-
-				return !FileSystem.File.Exists(filePath)
-					? null
-					: XDocument.Parse(FileSystem.File.ReadAllText(filePath));
-			}
-
-			XDocument data;
-
-			if (TryToLoadXDocumentFromCache(fileName, language, out data))
-				return data;
-
-			lock (Locker)
-			{
-				if (TryToLoadXDocumentFromCache(fileName, language, out data))
-					return data;
-
-				var filePath = GetFilePath(fileName, language);
-
-				if (FileSystem.File.Exists(filePath))
-				{
-					data = XDocument.Parse(FileSystem.File.ReadAllText(filePath));
-					XmlCache.Add(new KeyValuePair<string, string>(fileName, language), data);
-					return data;
-				}
-
-				filePath = GetFilePath(fileName, _defaultLanguage);
-
-				if (!FileSystem.File.Exists(filePath)) return null;
-
-				data = XDocument.Parse(FileSystem.File.ReadAllText(filePath));
-				XmlCache.Add(new KeyValuePair<string, string>(fileName, _defaultLanguage), data);
-				return data;
-			}
-		}
+		#region Text
 
 		/// <summary>
 		/// Load text from a file located in data folder
@@ -204,64 +130,108 @@ namespace Simplify.Web.Modules.Data
 		/// <exception cref="ArgumentNullException">fileName</exception>
 		public string LoadTextDocument(string fileName, string language, bool memoryCache = false)
 		{
-			if (string.IsNullOrEmpty(fileName)) throw new ArgumentNullException(nameof(fileName));
-
-			if (!memoryCache || _disableCache)
-			{
-				var filePath = GetFilePath(fileName, language);
-
-				if (FileSystem.File.Exists(filePath))
-					return FileSystem.File.ReadAllText(filePath);
-
-				filePath = GetFilePath(fileName, _defaultLanguage);
-
-				return !FileSystem.File.Exists(filePath)
-					? null
-					: FileSystem.File.ReadAllText(filePath);
-			}
+			if (string.IsNullOrEmpty(fileName))
+				throw new ArgumentNullException(nameof(fileName));
 
 			string data;
 
-			if (TryToLoadTextDocumentFromCache(fileName, language, out data))
+			if (!memoryCache || _disableCache)
+			{
+				if (LoadTextFileFromFileSystem(fileName, language, out data))
+					return data;
+
+				return LoadTextFileFromFileSystem(fileName, _defaultLanguage, out data) ? data : null;
+			}
+
+			if (LoadTextFileCached(fileName, language, out data))
 				return data;
 
+			return LoadTextFileCached(fileName, _defaultLanguage, out data) ? data : null;
+		}
+
+		#endregion Text
+
+		#region XML
+
+		/// <summary>
+		/// Load xml document from a file located in data folder
+		/// </summary>
+		/// <param name="fileName">File name</param>
+		/// <param name="memoryCache">Load file from memory cache if possible.</param>
+		/// <returns>
+		/// Xml document
+		/// </returns>
+		public XDocument LoadXDocument(string fileName, bool memoryCache = false)
+		{
+			return LoadXDocument(fileName, _languageManager.Language, memoryCache);
+		}
+
+		/// <summary>
+		/// Load xml document from a file with specific language located in data folder
+		/// </summary>
+		/// <param name="fileName">File name</param>
+		/// <param name="language">File language</param>
+		/// <param name="memoryCache">Load file from memory cache if possible.</param>
+		/// <returns>
+		/// Xml document
+		/// </returns>
+		/// <exception cref="ArgumentNullException">fileName</exception>
+		public XDocument LoadXDocument(string fileName, string language, bool memoryCache = false)
+		{
+			if (string.IsNullOrEmpty(fileName))
+				throw new ArgumentNullException(nameof(fileName));
+
+			if (!fileName.EndsWith(".xml"))
+				fileName = fileName + ".xml";
+
+			XDocument data;
+
+			if (!memoryCache || _disableCache)
+			{
+				if (LoadXDocumentFromFileSystem(fileName, language, out data))
+					return data;
+
+				return LoadXDocumentFromFileSystem(fileName, _defaultLanguage, out data) ? data : null;
+			}
+
+			if (LoadXDocumentCached(fileName, language, out data))
+				return data;
+
+			return LoadXDocumentCached(fileName, _defaultLanguage, out data) ? data : null;
+		}
+
+		#endregion XML
+
+		/// <summary>
+		/// Clears the cache.
+		/// </summary>
+		public static void ClearCache()
+		{
 			lock (Locker)
 			{
-				if (TryToLoadTextDocumentFromCache(fileName, language, out data))
-					return data;
-
-				var filePath = GetFilePath(fileName, language);
-
-				if (FileSystem.File.Exists(filePath))
-				{
-					data = FileSystem.File.ReadAllText(filePath);
-					TextCache.Add(new KeyValuePair<string, string>(fileName, language), data);
-					return data;
-				}
-
-				filePath = GetFilePath(fileName, _defaultLanguage);
-
-				if (!FileSystem.File.Exists(filePath)) return null;
-
-				data = FileSystem.File.ReadAllText(filePath);
-				TextCache.Add(new KeyValuePair<string, string>(fileName, _defaultLanguage), data);
-				return data;
+				XmlCache.Clear();
+				TextCache.Clear();
 			}
 		}
 
-		private bool TryToLoadXDocumentFromCache(string fileName, string language, out XDocument data)
+		private static bool TryToLoadTextFileFromCache(string fileName, string language, out string data)
+		{
+			data = null;
+
+			var cacheItem = TextCache.FirstOrDefault(x => x.Key.Key == fileName && x.Key.Value == language);
+
+			if (cacheItem.Equals(default(KeyValuePair<KeyValuePair<string, string>, string>)))
+				return false;
+
+			data = cacheItem.Value;
+			return true;
+		}
+
+		private static bool TryToLoadXDocumentFromCache(string fileName, string language, out XDocument data)
 		{
 			data = null;
 
 			var cacheItem = XmlCache.FirstOrDefault(x => x.Key.Key == fileName && x.Key.Value == language);
-
-			if (!cacheItem.Equals(default(KeyValuePair<KeyValuePair<string, string>, XDocument>)))
-			{
-				data = cacheItem.Value;
-				return true;
-			}
-
-			cacheItem = XmlCache.FirstOrDefault(x => x.Key.Key == fileName && x.Key.Value == _defaultLanguage);
 
 			if (cacheItem.Equals(default(KeyValuePair<KeyValuePair<string, string>, XDocument>)))
 				return false;
@@ -270,25 +240,66 @@ namespace Simplify.Web.Modules.Data
 			return true;
 		}
 
-		private bool TryToLoadTextDocumentFromCache(string fileName, string language, out string data)
+		private bool LoadTextFileFromFileSystem(string fileName, string language, out string data)
 		{
 			data = null;
 
-			var cacheItem = TextCache.FirstOrDefault(x => x.Key.Key == fileName && x.Key.Value == language);
+			var filePath = GetFilePath(fileName, language);
 
-			if (!cacheItem.Equals(default(KeyValuePair<KeyValuePair<string, string>, string>)))
-			{
-				data = cacheItem.Value;
-				return true;
-			}
-
-			cacheItem = TextCache.FirstOrDefault(x => x.Key.Key == fileName && x.Key.Value == _defaultLanguage);
-
-			if (cacheItem.Equals(default(KeyValuePair<KeyValuePair<string, string>, string>)))
+			if (!FileSystem.File.Exists(filePath))
 				return false;
 
-			data = cacheItem.Value;
+			data = FileSystem.File.ReadAllText(filePath);
 			return true;
+		}
+
+		private bool LoadTextFileCached(string fileName, string language, out string data)
+		{
+			if (TryToLoadTextFileFromCache(fileName, language, out data))
+				return true;
+
+			lock (Locker)
+			{
+				if (TryToLoadTextFileFromCache(fileName, language, out data))
+					return true;
+
+				if (!LoadTextFileFromFileSystem(fileName, language, out data))
+					return false;
+
+				TextCache.Add(new KeyValuePair<string, string>(fileName, language), data);
+
+				return true;
+			}
+		}
+
+		private bool LoadXDocumentFromFileSystem(string fileName, string language, out XDocument data)
+		{
+			data = null;
+
+			if (!LoadTextFileFromFileSystem(fileName, language, out string internaldata))
+				return false;
+
+			data = XDocument.Parse(internaldata);
+			return true;
+		}
+
+		private bool LoadXDocumentCached(string fileName, string language, out XDocument data)
+		{
+			if (TryToLoadXDocumentFromCache(fileName, language, out data))
+				return true;
+
+			lock (Locker)
+			{
+				if (TryToLoadXDocumentFromCache(fileName, language, out data))
+					return true;
+
+				if (!LoadXDocumentFromFileSystem(fileName, language, out data))
+					return false;
+
+				XmlCache.Add(new KeyValuePair<string, string>(fileName, language), data);
+
+				return true;
+			}
 		}
 	}
 }
