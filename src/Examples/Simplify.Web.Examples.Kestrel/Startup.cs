c650fec4463ec04e8f0d1aa9f91c71edfc285369
Alexanderius <alexander.krylkov@gmail.com>
2018-10-29 21:34:54 +0600

    Exclude Simplify.Web from exclude assemblies to be able to load example controllers

--- src/Examples/Simplify.Web.Examples.Kestrel/Startup.cs
+++ src/Examples/Simplify.Web.Examples.Kestrel/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Simplify.Web.Meta;
 using Simplify.Web.Owin;
 
 namespace Simplify.Web.Examples.Kestrel
@@ -8,6 +9,9 @@ namespace Simplify.Web.Examples.Kestrel
 	{
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
+			// Exclude Simplify.Web from exclude assemblies to be able to load example controllers
+			SimplifyWebTypesFinder.ExcludedAssembliesPrefixes.Remove("Simplify");
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
