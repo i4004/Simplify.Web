15550dac6cfc81cff09a6b42fdc99949b70327a0
Alexanderius <alexander.krylkov@gmail.com>
2018-11-04 12:05:28 +0600

    Refactored to Core

--- src/Simplify.Web.Tests/Modules/RedirectorTests.cs
+++ src/Simplify.Web.Tests/Modules/RedirectorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Owin;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 using Moq;
 using NUnit.Framework;
 using Simplify.Web.Modules;
@@ -12,19 +13,24 @@ namespace Simplify.Web.Tests.Modules
 	{
 		private Mock<IWebContext> _context;
 		private Redirector _redirector;
-		private Mock<IHeaderDictionary> _headerDictionary;
+		private Mock<IResponseCookies> _responseCookies;
 
 		[SetUp]
 		public void Initialize()
 		{
 			_context = new Mock<IWebContext>();
 			_redirector = new Redirector(_context.Object);
-			_headerDictionary = new Mock<IHeaderDictionary>();
+			_responseCookies = new Mock<IResponseCookies>();
 
 			_context.Setup(x => x.Response.Redirect(It.IsAny<string>()));
-			_context.Setup(x => x.Request.Uri).Returns(new Uri("http://localhost/mywebsite/myaction?=foo"));
-			_context.SetupGet(x => x.Response.Cookies).Returns(new ResponseCookieCollection(_headerDictionary.Object));
-			_context.Setup(x => x.SiteUrl).Returns("http://localhost/mywebsite/");
+
+			_context.SetupGet(x => x.Request.Scheme).Returns("http");
+			_context.SetupGet(x => x.Request.Host).Returns(new HostString("localhost"));
+			_context.SetupGet(x => x.Request.PathBase).Returns("/mywebsite");
+			_context.SetupGet(x => x.Request.Path).Returns("/myaction?=foo");
+			_context.SetupGet(x => x.SiteUrl).Returns("http://localhost/mywebsite/");
+
+			_context.SetupGet(x => x.Response.Cookies).Returns(_responseCookies.Object);
 			_context.SetupGet(x => x.Request.Cookies).Returns(new RequestCookieCollection(new Dictionary<string, string>()));
 		}
 
@@ -47,15 +53,15 @@ namespace Simplify.Web.Tests.Modules
 		[Test]
 		public void Redirect_ToRedirectUrlHaveRedirectUrl_RedirectCalledWithCorrectLinkPreviousNavigatedUrlSet()
 		{
-			// Arrange
+			// Assign
 
 			_context.SetupGet(x => x.Request.Cookies)
 				.Returns(new RequestCookieCollection(new Dictionary<string, string> { { Redirector.RedirectUrlCookieFieldName, "foo" } }));
 
-			_headerDictionary.Setup(x => x.AppendValues(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string[]>((key, values) =>
+			_responseCookies.Setup(x => x.Append(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string>((key, value) =>
 			{
-				Assert.AreEqual("Set-Cookie", key);
-				Assert.IsTrue(values[0].Contains(Redirector.PreviousNavigatedUrlCookieFieldName + "=" + Uri.EscapeDataString("http://localhost/mywebsite/myaction?=foo")));
+				Assert.AreEqual(Redirector.PreviousNavigatedUrlCookieFieldName, key);
+				Assert.AreEqual("http://localhost/mywebsite/myaction%3F=foo", value);
 			});
 
 			// Act
@@ -134,7 +140,7 @@ namespace Simplify.Web.Tests.Modules
 			_redirector.Redirect(RedirectionType.CurrentPage);
 
 			// Assert
-			_context.Verify(x => x.Response.Redirect(It.Is<string>(c => c == "http://localhost/mywebsite/myaction?=foo")), Times.Once);
+			_context.Verify(x => x.Response.Redirect(It.Is<string>(c => c == "http://localhost/mywebsite/myaction%3F=foo")), Times.Once);
 		}
 
 		[Test]
@@ -151,10 +157,10 @@ namespace Simplify.Web.Tests.Modules
 		public void SetRedirectUrlToCurrentPage_NormalUrl_Set()
 		{
 			// Assign
-			_headerDictionary.Setup(x => x.AppendValues(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string[]>((key, values) =>
+			_responseCookies.Setup(x => x.Append(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string>((key, value) =>
 			{
-				Assert.AreEqual("Set-Cookie", key);
-				Assert.IsTrue(values[0].Contains(Redirector.RedirectUrlCookieFieldName + "=" + Uri.EscapeDataString("http://localhost/mywebsite/myaction?=foo")));
+				Assert.AreEqual(Redirector.RedirectUrlCookieFieldName, key);
+				Assert.AreEqual("http://localhost/mywebsite/myaction%3F=foo", value);
 			});
 
 			// Act
@@ -166,12 +172,12 @@ namespace Simplify.Web.Tests.Modules
 		{
 			// Assign
 
-			_context.SetupGet(x => x.Request.Uri).Returns(new Uri("http://localhost/mywebsite/foo2"));
+			_context.SetupGet(x => x.Request.Path).Returns("/foo2");
 
-			_headerDictionary.Setup(x => x.AppendValues(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string[]>((key, values) =>
+			_responseCookies.Setup(x => x.Append(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string>((key, value) =>
 			{
-				Assert.AreEqual("Set-Cookie", key);
-				Assert.IsTrue(values[0].Contains(Redirector.LoginReturnUrlCookieFieldName + "=" + Uri.EscapeDataString("http://localhost/mywebsite/foo2")));
+				Assert.AreEqual(Redirector.LoginReturnUrlCookieFieldName, key);
+				Assert.AreEqual("http://localhost/mywebsite/foo2", value);
 			});
 
 			// Act
@@ -204,10 +210,10 @@ namespace Simplify.Web.Tests.Modules
 		public void SetPreviousPageUrl_Set()
 		{
 			// Assign
-			_headerDictionary.Setup(x => x.AppendValues(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string[]>((key, values) =>
+			_responseCookies.Setup(x => x.Append(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string>((key, value) =>
 			{
-				Assert.AreEqual("Set-Cookie", key);
-				Assert.IsTrue(values[0].Contains(Redirector.PreviousPageUrlCookieFieldName + "=foo"));
+				Assert.AreEqual(Redirector.PreviousPageUrlCookieFieldName, key);
+				Assert.AreEqual("foo", value);
 			});
 
 			// Act & Assert
