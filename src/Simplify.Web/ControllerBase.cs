afd24905a62e60ef17aece14552381cd1c42fd5a
Alexanderius <alexander.krylkov@gmail.com>
2018-12-06 00:00:01 +0600

    Ajax response wrapper

--- src/Simplify.Web/ControllerBase.cs
+++ src/Simplify.Web/ControllerBase.cs
@@ -18,6 +18,36 @@ namespace Simplify.Web
 		public virtual dynamic RouteParameters { get; internal set; }
 
 		/// <summary>
+		/// Initializes a new instance of the <see cref="Ajax" /> class.
+		/// </summary>
+		/// <param name="ajaxData">The ajax data.</param>
+		/// <param name="statusCode">The HTTP response status code.</param>
+		/// <param name="contentType">Type of the content.</param>
+		protected Ajax Ajax(string ajaxData, int statusCode = 200, string contentType = null)
+		{
+			return new Ajax(ajaxData, statusCode, contentType);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Ajax" /> class.
+		/// </summary>
+		/// <param name="ajaxData">The ajax data.</param>
+		/// <param name="contentType">Type of the content.</param>
+		protected Ajax Ajax(string ajaxData, string contentType)
+		{
+			return new Ajax(ajaxData, contentType);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Ajax" /> class.
+		/// </summary>
+		/// <param name="statusCode">The HTTP response status code.</param>
+		protected Ajax Ajax(int statusCode = 204)
+		{
+			return new Ajax(statusCode);
+		}
+
+		/// <summary>
 		/// Initializes a new instance of the <see cref="Responses.Tpl" /> class.
 		/// </summary>
 		/// <param name="template">The template.</param>
