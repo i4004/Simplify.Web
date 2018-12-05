a7d6857ab808ab20371029ac4e8c082fa6b4e9b5
Alexanderius <alexander.krylkov@gmail.com>
2018-12-05 23:57:06 +0600

    StaticTpl response wrapper

--- src/Simplify.Web/ControllerBase.cs
+++ src/Simplify.Web/ControllerBase.cs
@@ -1,4 +1,5 @@
-﻿using Simplify.Templates;
+﻿using System;
+using Simplify.Templates;
 using Simplify.Web.Responses;
 
 namespace Simplify.Web
@@ -36,6 +37,18 @@ namespace Simplify.Web
 		protected Tpl Tpl(string data, string title = null, int statusCode = 200)
 		{
 			return new Tpl(data, title, statusCode);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Responses.Tpl" /> class.
+		/// </summary>
+		/// <param name="templateFileName">Name of the template file.</param>
+		/// <param name="title">The title.</param>
+		/// <param name="statusCode">The HTTP response status code.</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		protected StaticTpl StaticTpl(string templateFileName, string title = null, int statusCode = 200)
+		{
+			return new StaticTpl(templateFileName, title, statusCode);
 		}
 	}
 }
