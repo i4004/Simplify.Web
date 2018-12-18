6ed7bcc3880a44c0e4e86fcace0a93028e268065
Alexanderius <alexander.krylkov@gmail.com>
2018-12-18 18:28:39 +0600

    Simplify.Web ASP.NET Core based example hosted on Windows service

---
+++ src/Examples/Simplify.Web.Examples.WindowsService/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Simplify.Web.Owin;
+
+namespace Simplify.Web.Examples.WindowsService
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseSimplifyWeb();
+		}
+	}
+}
