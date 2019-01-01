ac319b4f4e9da15c3b75879cbe610a9831596e42
Alexanderius <alexander.krylkov@gmail.com>
2019-01-01 17:01:32 +0600

    Vue sample app added

---
+++ src/SampleApps/SampleApp.Vue/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Simplify.Web;
+using Simplify.Web.Attributes;
+
+namespace SampleApp.Vue.Controllers
+{
+	[Get("/")]
+	public class DefaultController : Controller
+	{
+		public override ControllerResponse Invoke()
+		{
+			return Tpl("");
+		}
+	}
+}
